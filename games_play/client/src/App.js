a9595769b3ada36749cec3694eaf7608aed87efb
Flu3nt <gnedyalkov94@gmail.com>
2023-03-16 08:44:34 +0200

    Extract all components and install react router

--- games_play/client/src/App.js
+++ games_play/client/src/App.js
@@ -1,9 +1,22 @@
 import { Header } from "./components/Header/Header";
+import { Footer } from "./components/Footer/Footer";
+import { Home } from "./components/Home/Home";
+import { Login } from "./components/Login/Login";
+import { Register } from "./components/Register/Register";
+import { Catalogue } from "./components/Catalogue/Catalogue";
+import { CreateGame } from "./components/CreateGame/CreateGame";
 
 function App() {
   return (
     <div id="box">
       <Header />
+      <main id="main-content">
+        {/* <Home /> */}
+        {/* <Login /> */}
+        {/* <Register /> */}
+        {/* <Catalogue /> */}
+      </main>
+      <Footer />
     </div>
   );
 }
