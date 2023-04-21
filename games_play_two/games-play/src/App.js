03e470f702bb7197d3ff88fa14f492206f1abbaa
Flu3nt <gnedyalkov94@gmail.com>
2023-04-21 11:22:25 +0300

    Extract components

--- games_play_two/games-play/src/App.js
+++ games_play_two/games-play/src/App.js
@@ -1,7 +1,19 @@
+import { Footer } from "./components/Footer/Footer";
+import { Header } from "./components/Header/Header";
+import { Home } from "./components/Home/Home";
+
 function App() {
   return (
     <div className="App">
-      <div id="box"></div>
+      <Header />
+
+      <div id="box">
+        <main id="main-content">
+          <Home />
+        </main>
+      </div>
+
+      <Footer />
     </div>
   );
 }
---
+++ games_play_two/games-play/src/components/Footer/Footer.js
@@ -0,0 +1,3 @@
+export const Footer = () => {
+  return <footer>copy&; games play demo app</footer>;
+};
