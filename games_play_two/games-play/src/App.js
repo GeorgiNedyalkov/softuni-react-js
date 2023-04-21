34e29dcddc8a4a98aae7958b675fc8b5d5c8a5a9
Flu3nt <gnedyalkov94@gmail.com>
2023-04-21 11:54:31 +0300

    Add routes and navigation to the routes

--- games_play_two/games-play/src/App.js
+++ games_play_two/games-play/src/App.js
@@ -1,15 +1,30 @@
-import { Footer } from "./components/Footer/Footer";
-import { Header } from "./components/Header/Header";
+import { useState, useEffect } from "react";
+import { Routes, Route } from "react-router-dom";
+
 import { Home } from "./components/Home/Home";
+import { Header } from "./components/Header/Header";
+import { Footer } from "./components/Footer/Footer";
+import { Login } from "./components/Login/Login";
+import { Register } from "./components/Register/Register";
+import { CreateGame } from "./components/CreateGame/CreateGame";
+import { Catalogue } from "./components/Catalogue/Catalogue";
 
 function App() {
+  const [games, setGames] = useState([]);
+
   return (
     <div className="App">
       <Header />
 
       <div id="box">
         <main id="main-content">
-          <Home />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/create-game" element={<CreateGame />} />
+            <Route path="/catalogue" element={<Catalogue />} />
+          </Routes>
         </main>
       </div>
 
